2ea1c8fba72846cbec328889d1d645910a47c861
jer <jer@moo>
2021-10-20 13:24:50 +0000

    initial commit

---
+++ lib/ueberauth_webauthn.ex
@@ -0,0 +1,18 @@
+defmodule UeberauthWebauthn do
+  @moduledoc """
+  Documentation for `UeberauthWebauthn`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UeberauthWebauthn.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ueberauth_webauthn_test.exs
@@ -0,0 +1,8 @@
+defmodule UeberauthWebauthnTest do
+  use ExUnit.Case
+  doctest UeberauthWebauthn
+
+  test "greets the world" do
+    assert UeberauthWebauthn.hello() == :world
+  end
+end
